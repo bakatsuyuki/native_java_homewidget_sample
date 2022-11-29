2def5cf65e34a4e56f4088ec734c30990d23b5f6
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-30 08:46:02 +0900

    修正

--- app/src/main/java/com/bakatsuyuki/myapplication/ExampleAppWidgetProvider.java
+++ app/src/main/java/com/bakatsuyuki/myapplication/ExampleAppWidgetProvider.java
@@ -29,4 +29,8 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
+
+    public void updateAppWidget(){
+
+    }
 }
